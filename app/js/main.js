12f297f3e847cc68753f9046f33a07597ef7824a
AndrewLymar <andriilymar@gmail.com>
2017-09-30 23:43:39 +0300

    new version

---
+++ app/js/main.js
@@ -0,0 +1,36 @@
+$(function() {
+    var menuIsOpened = false;
+    
+    $(window).on("resize", function() {
+        if (!menuIsOpened && $(window).width() > "768") {
+            showMenu();
+        }
+        else if (menuIsOpened && $(window).width() < "768") {
+            hideMenu();
+        }
+    });
+
+    $(".mobile-menu-icon").on("click", function() {
+        if (!menuIsOpened) {
+            showMenu();
+        } else {
+            hideMenu();
+        }
+    });
+    $("nav a").on("click", function() {
+        if ($(window).width() <= "768") {
+            hideMenu();
+        }
+    });
+
+    function showMenu() {
+        $("nav ul").css("display", "flex");
+        menuIsOpened = true;
+    }
+    
+    function hideMenu() {
+         $("nav ul").css("display", "none");
+         menuIsOpened = false;
+    }
+    
+});
